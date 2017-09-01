41f9c4e5b7c5e3216e845a4a29a38b7b0bf1aca2
e <1456958517@qq.com>
2017-09-01 16:10:44 +0800

    over

--- test.js
+++ test.js
@@ -1,11 +1,16 @@
 var array = [5,8,9,50,3,4,10,100];
-var result = array.sort(function (a,b) {
-    return a - b ;
-})
-if(result.length%2 === 0){
-    var i = result.length / 2;
-    console.log((result[i - 1]+ result[i]) /2);
-}else{
-    var i = (result.length - 1) /2;
-    console.log(result[i]);
+
+function main(array) {
+    var result = array.sort(function (a, b) {
+        return a - b;
+    })
+    if (result.length % 2 === 0) {
+        var i = result.length / 2;
+        console.log((result[i - 1] + result[i]) / 2);
+    } else {
+        var i = (result.length - 1) / 2;
+        console.log(result[i]);
+    }
 }
+
+main(array);
